07ddd3d67c234c86e2001ce7bcd04670d822fdfc
stefant989 <stefant989@gmail.com>
2024-06-09 15:36:55 +0200

    Playground

---
+++ playground/src/App.tsx
@@ -0,0 +1,25 @@
+import { useScreenSize } from 'reactinity'
+
+function App() {
+	const screen = useScreenSize()
+
+	return (
+		<>
+			{screen.name === 'MOBILE' ? (
+				<h2 className='font-50 purple-dark'>MOBILE</h2>
+			) : screen.name === 'LANDSCAPE' ? (
+				<h2 className='font-50 purple-dark'>LANDSCAPE</h2>
+			) : screen.name === 'TABLET' ? (
+				<h2 className='font-50 red-light'>TABLET</h2>
+			) : screen.name === 'LAPTOP' ? (
+				<h2 className='font-50 yellow'>LAPTOP</h2>
+			) : screen.name === 'DESKTOP' ? (
+				<h2 className='font-50 blue-dark'>DESKTOP</h2>
+			) : screen.name === 'TV' ? (
+				<h2 className='font-50 gray-dark'>TV</h2>
+			) : null}
+		</>
+	)
+}
+
+export default App
